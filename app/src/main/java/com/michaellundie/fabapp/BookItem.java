8b84d7e27367a501d1c5e4598dbefa96ad7fc0c8
Michael Lundie <michael@tal.tokyo>
2018-04-29 00:19:12 +0900

    Initial commit

---
+++ app/src/main/java/com/michaellundie/fabapp/BookItem.java
@@ -0,0 +1,8 @@
+package com.michaellundie.fabapp;
+
+/*
+  This is a custom object class for
+ */
+public class BookItem {
+
+}
---
+++ app/src/main/java/com/michaellundie/fabapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.michaellundie.fabapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String GBOOKS_REQUEST_URL =
+            "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
