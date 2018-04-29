2ef6b80e9ee0257b5ab3023dacafc8a5f7c2ccdf
Michael Lundie <michael@tal.tokyo>
2018-04-29 09:49:37 +0900

    Added simple BookItem class. No Frills.

--- app/src/main/java/com/michaellundie/fabapp/BookItem.java
+++ app/src/main/java/com/michaellundie/fabapp/BookItem.java
@@ -4,5 +4,25 @@ package com.michaellundie.fabapp;
   This is a custom object class for
  */
 public class BookItem {
+    private String title;
+    private String author;
+    private int thumbnailResourceId;
 
+    public BookItem(String title, String author, int thumbnailResourceId) {
+        this.title = title;
+        this.author = author;
+        this.thumbnailResourceId = thumbnailResourceId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getThumbnailResourceId() {
+        return thumbnailResourceId;
+    }
 }
